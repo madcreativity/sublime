1be857850d49ae2445464a28c49364232ea3b7b2
MadCreativity <ahlgreenmadsen@gmail.com>
2019-04-22 18:36:44 +0200

    Add searchbar dropdown

--- components/nav/assets/js/component_nav.js
+++ components/nav/assets/js/component_nav.js
@@ -1,13 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
-    /// Gather elements
+    // Gather elements
     var nav = document.querySelector(".nav-header__nav");
 
-    /// Update navbar size upon page scroll
+    var searchButton = document.querySelector(".nav-header__nav__search-checkbox");
+    var searchBar = document.querySelector(".nav-header__search-dropdown-container");
+
+    // Update navbar size upon page scroll
     window.addEventListener("scroll", function(e) {
         if(window.scrollY >= 70) {
             nav.classList.add("nav-header__nav--small");
         } else {
             nav.classList.remove("nav-header__nav--small");
+        }
+    });
+
+    // Search bar
+    searchButton.addEventListener("change", function() {
+        if(searchButton.checked) {
+            searchBar.classList.add("nav-header__search-dropdown-container--open");
+        } else {
+            searchBar.classList.remove("nav-header__search-dropdown-container--open");
         }
     });
 });
