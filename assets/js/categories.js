b5fe203b50a98e2bf1104fde277dd3abcfc909b5
MadCreativity <ahlgreenmadsen@gmail.com>
2019-05-02 13:12:19 +0200

    Add newsletter section and icon box section

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Non-DOM-element variables
+    var productResultAmount = 0;
+
     // Gather elements
-    var productResultAmount = document.querySelector(".main__content__products__top-container__results__result-amount");
+    var DOMproductResultAmount = document.querySelector(".main__content__products__top-container__results__result-amount");
+
+    DOMproductResultAmount.textContent = productResultAmount;
 });
