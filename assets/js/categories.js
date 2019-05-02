6acf028fede26e086e8e07157b3fe31dc2872639
MadCreativity <ahlgreenmadsen@gmail.com>
2019-05-02 11:22:54 +0200

    Change outputted result amount to be variable

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+    // Gather elements
+    var productResultAmount = document.querySelector(".main__content__products__top-container__results__result-amount");
 });
